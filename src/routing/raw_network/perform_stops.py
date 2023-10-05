ca2f3e787bc7784bf4a99fc7d9101e7f0930c791
Gagan Shenvi <107752760+imgagan18@users.noreply.github.com>
2023-10-06 03:32:38 +0530

    Update perform_stops.py
    optimised code

--- src/routing/raw_network/perform_stops.py
+++ src/routing/raw_network/perform_stops.py
@@ -1,6 +1,5 @@
-import pandas
-import geopandas
-
+import pandas as pd
+import geopandas as gpd
 
 def MakeStops(intersections_df, meters=800):
     '''
@@ -9,30 +8,29 @@ def MakeStops(intersections_df, meters=800):
     OUTPUT: DataFrame
     '''
 
-    # Sort the DataFrme in order of descending population
+    # Sort the DataFrame in order of descending population
     iPop_df = intersections_df[intersections_df.iPop > 0].sort_values('iPop', ascending=False)
 
-    indexList = []
-    served = [] # population served at each bus stop identified
-    while iPop_df.shape[0] != 0:
-        stop = iPop_df.geometry.values[0]
-        idx = iPop_df.index.values[0]
-        indexList.append(idx)
-        # identify the exclusion zone
-        zone = stop.buffer(meters) # 400 meters = approx 1/4 mile
-        # reach is the sum of all populations in the exclusion zone
-        reach = iPop_df[iPop_df.geometry.intersects(zone) == True].iPop.sum()
-        served.append([idx, reach])
-        #filter out any intersections within exclusion zone
-        iPop_df = iPop_df[iPop_df.geometry.intersects(zone) == False]
-
-    stops_df = intersections_df[intersections_df.index.isin(indexList)]
-
-    # convert the 'served' array into a DataFrame
-    served_df = pandas.DataFrame(data=served, columns=['idx','mergedPop'], dtype='int')
-    served_df.set_index('idx', inplace=True)
+    stops_indices = []
+    served_populations = []
     
-    # Merge DataFrames
-    stops_df = stops_df.merge(served_df, left_index=True, right_index=True)
+    while not iPop_df.empty:
+        stop = iPop_df.iloc[0]
+        idx = stop.name
+        stops_indices.append(idx)
+        
+        # Identify the exclusion zone
+        zone = stop.geometry.buffer(meters)
+        
+        # Calculate the total population served in the exclusion zone
+        reach = iPop_df[iPop_df.geometry.intersects(zone)]['iPop'].sum()
+        served_populations.append(reach)
+        
+        # Filter out intersections within the exclusion zone
+        iPop_df = iPop_df[~iPop_df.geometry.intersects(zone)]
+
+    stops_df = intersections_df.loc[stops_indices]
+    stops_df['mergedPop'] = served_populations
 
     return stops_df
+
